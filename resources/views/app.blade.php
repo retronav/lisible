4c8357bc5cd28b934095ab30b5b25b5ff9d80fd5
Pranav Karawale <pranav@karawale.in>
2025-09-01 23:18:59 +0530

    Update font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=plus-jakarta-sans:400,500,600" rel="stylesheet" />
 
     @routes
     @vite(['resources/js/app.ts'])
